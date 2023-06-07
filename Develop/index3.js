2d0c4f4356a2ae9b78fe5945e791c8909a94f299
lexslie <lesliiee727@gmail.com>
2023-06-07 12:46:10 -0700

    Updated index3.js
    adjusted content of "name: " under const questions to get rid of 'undefinded' error when README was being generated

--- Develop/index3.js
+++ Develop/index3.js
@@ -1,54 +1,54 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+// const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {type: "input",
+    {type: 'input',
     message: "What is the title of your project?",
-    name: "Title",
+    name: "title",
     },
     {type: "input",
     message: "Provide a brief description of your project.",
-    name: "Description",
+    name: "description",
     },
     {type: "input",
     message: "What is the application's installation process/instructions?",
-    name: "Installation instructions",
+    name: "installation",
     },
     {type: "input",
     message: "What information does the user need to know about usage of the application?",
-    name: "Usage",
+    name: "usage",
     },
     {type: "input",
     message: "What guidelines must the user follow when contributing to the project/application?",
-    name: "Contributing",
+    name: "contributors",
     },
     {type: "input",
     message: "What instructions must the user follow when testing the application?",
-    name: "Tests",
+    name: "tests",
     },
     {type: "list",
     message: "Under which license is your application covered?",
     choices: ["apache", "ms-pl", "BSD", "MIT"],
-    name: "License",
+    name: "license",
     },
     {type: "input",
     message: "What is your Github username?",
-    name: "Github username",
+    name: "username",
     },
     {type: "input",
     message: "What is your email address?",
-    name: "Email address",
+    name: "email",
     },   
 ];
 
 
 // Function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
+function writeToFile(fileName, readmeContent) {
+    fs.writeFile(fileName, readmeContent, (err) => {
         if (err) {
           console.error(err);
           return;
@@ -62,15 +62,43 @@ function init() {
     inquirer.prompt(questions).then(answers => {
         const { title, description, installation, usage, contributors, tests, license, username, email } = answers;
   
-        let readmeContent = `# ${title}\n\n`;
-        readmeContent += `## Description\n\n${description}\n\n`;
-        readmeContent += `## Installation\n\n${installation}\n\n`;
-        readmeContent += `## Usage\n\n${usage}\n\n`;
-        readmeContent += `## Contributors\n\n${contributors}\n\n`;
-        readmeContent += `## Tests\n\n${tests}\n\n`;
-        readmeContent += `## License\n\n${license}\n\n`;
-        readmeContent += `## Username\n\n${username}\n\n`;
-        readmeContent += `## Email\n\n${email}\n\n`;
+        // let readmeContent = `# ${title}\n\n`;
+        // readmeContent += `## Description\n\n${description}\n\n`;
+        // readmeContent += `## Installation\n\n${installation}\n\n`;
+        // readmeContent += `## Usage\n\n${usage}\n\n`;
+        // readmeContent += `## Contributors\n\n${contributors}\n\n`;
+        // readmeContent += `## Tests\n\n${tests}\n\n`;
+        // readmeContent += `## License\n\n${license}\n\n`;
+        // readmeContent += `## Username\n\n${username}\n\n`;
+        // readmeContent += `## Email\n\n${email}\n\n`;
+
+        const readmeContent = `
+# ${title}
+
+## Description
+${description}
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributers
+${contributors}
+
+## Tests
+${tests}
+
+## License 
+${license}
+
+## GitHub Username
+${username}
+
+## Email
+${email}
+`;
 
         writeToFile('README.md', readmeContent, (err) => {
             if (err) {
